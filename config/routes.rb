a31b66bb1fe5e8debafbc4a3ba0bff4d8965bd9f
Mettralla <daxter0493@gmail.com>
2022-12-17 16:39:00 -0300

    User authentication added

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  get 'events/index'
+  root 'events#index'
+  devise_for :users
+  resources :events, only: %i[index]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
